5875a412976e96d4d9ce737f8bb9aa6520c37e9b
luyuanliang <luyuanliang_1984@163.com>
2021-10-10 10:55:33 +0800

    update

--- example-springcloud/src/test/java/org/web/example/springcloud/NotifyTest.java
+++ example-springcloud/src/test/java/org/web/example/springcloud/NotifyTest.java
@@ -6,6 +6,9 @@ public class NotifyTest {
 
     public static void main(String[] args) {
         System.out.println("begin");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
         NotifyThread test = new NotifyThread();
         test.start();
         test.synFunction2();
