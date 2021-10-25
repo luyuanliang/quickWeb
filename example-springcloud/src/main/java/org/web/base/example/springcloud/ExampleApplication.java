dce71a2db83d2da6f7d25b5aa0dbdc5d318f04a6
luyuanliang <luyuanliang_1984@163.com>
2021-10-25 21:49:39 +0800

    add demo.

--- example-springcloud/src/main/java/org/web/base/example/springcloud/ExampleApplication.java
+++ example-springcloud/src/main/java/org/web/base/example/springcloud/ExampleApplication.java
@@ -1,12 +1,13 @@
 package org.web.base.example.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication()
 //@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-//@MapperScan(basePackages = {"org.web.example.springcloud.dao"})
+
 @ImportResource({"classpath:mapper/mybatis-config-all.xml"})
 public class ExampleApplication {
 
